800f9d696a55cf9d78fff644eb561c4ed3d89ba6
kelvin.andrade <kelvin.andrade@protechsolutions.com.br>
2023-08-20 16:05:00 -0300

    Adicionar arquivos de projeto.

---
+++ Models/CepViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsumoCEPWS.Models
+{
+    public class CepViewModel
+    {
+        public string? Rua { get; set; }
+        public int NumeroCasa { get; set; }
+        public int CEP { get; set; }
+        public string? Complemento { get; set; }
+        public string? Cidade { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumoCEPWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
